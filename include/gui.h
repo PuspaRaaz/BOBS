7bc9e9f9ee1b29f91b3ae39416e42b964661a632
पुष्प राज <social@pusparaaz.com.np>
2015-03-21 18:05:35 +0545

    multiple defn solved

--- include/gui.h
+++ include/gui.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int STARTOFCODE = 0x8000;
+#define STARTOFCODE 0x8000
 GtkWidget *textAreaYourCode, *textAreaConvertedCode, *errorArea, *notebook, *memLocation, *memValue;
 GtkWidget *flagEntry[5], *registerEntry[9], *ioPortEntry[3];
 PangoFontDescription* font_desc;
